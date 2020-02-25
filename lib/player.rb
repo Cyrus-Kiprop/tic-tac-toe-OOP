700a51885e5a76866b9fe485e6c49e2a754540b9
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2020-02-26 00:01:18 +0300

    Model the player class

---
+++ lib/player.rb
@@ -0,0 +1,29 @@
+require './lib/tictactoe.rb'
+
+class Player < TicTacToe
+  attr_reader :player
+
+  def initialize(player_one, player_two)
+    @player_one = player_one
+    @player_two = player_two
+
+    @player = [
+      {
+        name: @player_one,
+        token: 'X'
+      },
+      {
+        name: @player_two,
+        token: 'O'
+      }
+    ]
+  end
+
+  def make_move; end
+
+  def play; end
+end
+
+game_play = Player.new('Alexis Sanchez', 'Cyrus Kiprop')
+
+puts game_play.player
